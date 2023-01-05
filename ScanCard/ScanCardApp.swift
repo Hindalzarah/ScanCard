54d81c43a17ce38ac2419fa0de940e24a97afaab
Hind <hindalzarah@outlook.com>
2023-01-05 10:42:21 +0300

    Initial Commit

---
+++ ScanCard/ScanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanCardApp.swift
+//  ScanCard
+//
+//  Created by Hind on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
